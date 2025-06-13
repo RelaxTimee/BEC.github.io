06d1acef64a2637c50a97e20a6231e2e3ab612d2
RelaxTimee <pawitteam201810@gmail.com>
2025-06-13 23:35:53 +0700

    New update UI (Navbar,Home)

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Navbar.css';
+import { FaInstagram, FaLinkedin, FaCommentDots } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
@@ -14,9 +15,15 @@ const Navbar = () => {
         <div className="navbar-links">
           <a className="navbar-link" href="/about">About</a>
           <a className="navbar-link" href="/speakers">Speakers</a>
-          <a className="navbar-link" href="/agenda">Agenda</a>
           <a className="navbar-link" href="/location">Location</a>
           <a className="navbar-link" href="/sponsors">Sponsors</a>
+        </div>
+
+        {/* Social Icons */}
+        <div className="navbar-social">
+          <a href="https://instagram.com" target="_blank" rel="noreferrer"><FaInstagram /></a>
+          <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+          <a href="/contact"><FaCommentDots /></a>
         </div>
       </div>
     </nav>
