f763f63acd71d5737f90603e5c001e6f43caf171
RelaxTimee <pawitteam201810@gmail.com>
2025-06-14 00:29:41 +0700

    update last to day

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Navbar.css';
-import { FaInstagram, FaLinkedin, FaCommentDots } from 'react-icons/fa';
+import { FaInstagram, FaLinkedin, FaLine } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
@@ -13,17 +13,17 @@ const Navbar = () => {
 
         {/* Navigation Links */}
         <div className="navbar-links">
-          <a className="navbar-link" href="/about">About</a>
-          <a className="navbar-link" href="/speakers">Speakers</a>
-          <a className="navbar-link" href="/location">Location</a>
-          <a className="navbar-link" href="/sponsors">Sponsors</a>
+          <a className="navbar-link" href="#about">About</a>
+          <a className="navbar-link" href="#speakers">Team us</a>
+          <a className="navbar-link" href="#agenda">Event</a>
+          <a className="navbar-link" href="#sponsors">Sponsors</a>
         </div>
 
         {/* Social Icons */}
         <div className="navbar-social">
-          <a href="https://instagram.com" target="_blank" rel="noreferrer"><FaInstagram /></a>
-          <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedin /></a>
-          <a href="/contact"><FaCommentDots /></a>
+          <a href="https://www.instagram.com/su.beclub/" target="_blank" rel="noreferrer"><FaInstagram /></a>
+          <a href="https://www.linkedin.com/company/bangrak-entrepreneur-club/posts/?feedView=all" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+          <a href="https://shorturl.at/MZcVr "><FaLine /></a>
         </div>
       </div>
     </nav>
